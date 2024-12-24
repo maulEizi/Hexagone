ed7286cfb94498588142d7f4f2e2bcce379322c2
maulEizi <Pauldubled@outlook.fr>
2024-12-24 11:54:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Playing Brest", "Playing Brest"];
+const statusMessages = ["Joue à Brest RP", "Par Snow"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
